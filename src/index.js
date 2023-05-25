8f286d1ff13549f6d0a6e9cfe87675805a4f17af
DanteW <dante.weverbergh@audience-advantage.com>
2023-05-25 21:59:14 +0200

    landing page dark mode

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const contactHeaderButton = document.getElementById('contactBtn');
+
+contactHeaderButton.addEventListener('click', function () {
+  window.location.href = 'mailto:chris@vertonghen.com';
+});
